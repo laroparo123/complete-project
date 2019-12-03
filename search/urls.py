9e73aef0dc6a664f6fc8218e5dee78c284b1be20
PrabhavLamichhane <parolaro123@gmail.com>
2019-12-03 13:31:18 +0545

    version-0

---
+++ search/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from .views import (
+        SearchProductView
+        )
+
+app_name = 'search'
+
+urlpatterns = [
+    url(r'^$', SearchProductView.as_view(), name='query'),
+]
+
